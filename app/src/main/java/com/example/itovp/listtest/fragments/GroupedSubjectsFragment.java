59c38d8005f518e48aa870a5a52fac6ea0696bfa
itovp <itovp@softserveinc.com>
2017-02-24 18:45:50 +0200

    exp list view, animations, fragments

---
+++ app/src/main/java/com/example/itovp/listtest/fragments/GroupedSubjectsFragment.java
@@ -0,0 +1,20 @@
+package com.example.itovp.listtest.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by itovp on 23.02.2017.
+ */
+
+public class GroupedSubjects extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater lf, ViewGroup vg, Bundle bundle) {
+
+		return null;
+	}
+}
